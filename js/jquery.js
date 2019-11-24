322cec5d2c8b4662290efc02dbe348cef6867b59
NguyenYVan <nguyenduc231795@gmail.com>
2019-11-25 01:10:52 +0700

    doing nhap lieu page

---
+++ js/jquery.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+  // swiper
+  var swiper = new Swiper('.swiper-container');
+  // wow
+  wow = new WOW(
+    {
+      animateClass: 'animated',
+      offset: 100,
+      callback: function(box) {
+        console.log("WOW: animating <" + box.tagName.toLowerCase() + ">")
+      }
+    }
+  );
+  wow.init();
+  // menu
+  $(".menubar").click(function () { 
+    $("body").addClass("menuactive");
+    $(".header__title").addClass("left65");
+  });
+  $(".menu__close").click(function () { 
+    $("body").removeClass("menuactive");
+    $(".header__title").removeClass("left65");
+  });
+});
